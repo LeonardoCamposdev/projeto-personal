f95f4be2e1a4568b00c5c58cb295a671ca2ff8c2
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-10-04 20:38:00 -0300

    updtate

--- js/modules/menu-interno-scroll.js
+++ js/modules/menu-interno-scroll.js
@@ -1,21 +1,25 @@
 export default function initMenuInternoScroll() {
-}
+  const linksInternos = document.querySelectorAll('[data-menu="list"] a');
 
 
-const linksInternos = document.querySelectorAll('[data-menu="list"] a');
-
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
+if(linksInternos){
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
   
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  })
+  
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
 }
 
 
-linksInternos.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
+
--- js/scrpt.js
+++ js/scrpt.js
@@ -1,5 +1,9 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import initMenuInternoScroll from "./modules/menu-interno-scroll.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initPluginScroll from "./modules/plugin-scroll.js";
 
 initMenuMobile();
 initMenuInternoScroll();
+initAnimacaoScroll();
+initPluginScroll();
