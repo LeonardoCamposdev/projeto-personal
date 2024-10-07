066cfb06b054a003ce0285a870adb054fde8a9ff
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-10-07 18:47:52 -0300

    up

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,3 +1,17 @@
 export default function initAnimacaoScroll(){
-
+  const scrollLeft = document.querySelectorAll('[data-anime="scroll-left"]');
+  const windowMetade = window.innerHeight * 0.6;
+  function animaScroll(){
+    scrollLeft.forEach((item) =>{
+      const itemTop = item.getBoundingClientRect().top - windowMetade;
+      if(itemTop < 0){
+        item.classList.add('ativo');
+      }
+    })
+  }
+  animaScroll();
+  
+  window.addEventListener('scroll', animaScroll)
+  
 }
+
